f57c0fd0dd033fda4b1fe7bce83252d5fb0c03f7
Suyash_Kakade <kakadesuyash1@gmail.com>
2025-02-13 19:37:11 +0530

    added the middleware and rest api

---
+++ Rest API/index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const users= require('./MOCK_DATA.json');
+const fs = require('fs');
+const app = express();
+const port = 8080;
+// routes
+// REST API
+
+app.use(express.urlencoded({extended:false}));
+
+app.get('/api/users',(req,res)=>{
+    return res.json(users);
+});
+
+app.get('/users',(req,res)=>{
+    // rendering with HTML 
+    const html=`<ul> ${users.map((user)=>`<li>${user.first_name} ${user.last_name}</li>`).join('')}</ul>`; 
+    res.send(html); 
+})
+
+// :id -> dynamically id  , gives the specific id user
+// http://localhost:8080/api/users/1
+
+
+app.get('/api/users/:id',(req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user)=> user.id===id);
+    return res.json(user);
+})
+app.post('/api/users', (req, res) => {
+    const body = req.body;
+    users.push({ ...body, id: users.length + 1 });
+
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err) => {
+        if (err) {
+            return res.status(500).json({ status: "error", message: "Failed to write data" });
+        }
+        return res.json({ status: "success", id: users.length }); // return the id of the user
+    });
+});
+
+
+app.path('/api/users/:id',(req,res)=>{
+    // TODO edit user with id
+    return  res.json({status:"pending"});
+})
+
+app.delete('/api/users/:id',(req,res)=>{    
+    // TODO delete user with id
+    return  res.json({status:"pending"});
+})
+
+// as we can see the '/api/users/:id' is common in all the routes
+// we can do it like this
+// chaining the routes
+app.route('/api/users/:id')
+.get((req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user)=> user.id===id);
+    return res.json(user);
+})
+.post((req,res)=>{
+    // TODO create new user
+   return  res.json({status:"pending"});
+})
+.patch((req,res)=>{
+    // TODO edit user with id
+    return  res.json({status:"pending"});
+})  
+.delete((req,res)=>{
+    // TODO delete user with id
+    return  res.json({status:"pending"});
+})
+
+app.listen(port,()=>console.log(`server is startes on http://localhost:${port}`));
---
+++ middleware/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port  = 8000;
+
+// middldeware 1 
+app.use((req,res,next)=>{
+    console.log('middleware 1');
+    next(); //here next id passes the next middleware or route , if we dont pass
+    //  next() then the request will be stuck here
+})
+
+
+app.use((req,res,next)=>{
+
+})
+app.get('/',(req,res)=>{
+    res.send('Hello world');
+})
+
+app.get('/about',(req,res)=>{
+    res.send('Hello about');
+})
+
+app.get('/contact',(req,res)=>{
+    res.send('Hello contact');
+})
+app.listen(port,()=>{
+    console.log(`server is running on the http://localhost:${port}`);
+})
