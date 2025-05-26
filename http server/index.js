b5b7a392009e60ca2a4ab8ca0318f29e560c473f
Suyash_Kakade <kakadesuyash1@gmail.com>
2025-05-26 15:54:04 +0530

    new try

--- http server/index.js
+++ http server/index.js
@@ -1,14 +1,26 @@
-console.log('program started')
-const { log } = require('console');
-const http = require('http');
-const myserver = http.createServer((req,res)=>{
-    console.log("new request receives");
-    console.log(req.headers);
+// console.log('program started')
+// const { log } = require('console');
+// const http = require('http');
+// const myserver = http.createServer((req,res)=>{
+//     console.log("new request receives");
+//     console.log(req.headers);
 
-    res.end("<h1>hello form server</h1>");
+//     res.end("<h1>hello form server</h1>");
+// })
+
+// // port number required to run the 
+// const port = 8001;
+// myserver.listen(port,()=>console.log(`server started on http://localhost:${port}`));
+
+console.log('program started');
+const {log} = require('console');
+const http = require('http');
+const myserver1 = http.createServer((req,res)=>{
+    console.log('new request receives');
+    console.log(req.headers);
+    res.end('<h1>hello from server</h1>');
 })
 
-// port number required to run the 
-const port = 8001;
-myserver.listen(port,()=>console.log(`server started on http://localhost:${port}`));
+const port1 = 9000;
+myserver1.listen(port1,()=>console.log('server started on http://localhost:'+port1));
 
